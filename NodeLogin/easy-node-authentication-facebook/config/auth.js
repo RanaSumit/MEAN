6bdad49b611b5fed819918f7cde095b1ab7857ab
RanaSumit <rana91sumit@gmail.com>
2016-01-27 23:07:14 -0800

    Node Js All Login

---
+++ NodeLogin/easy-node-authentication-facebook/config/auth.js
@@ -0,0 +1,23 @@
+
+// expose our config directly to our application using module.exports
+module.exports = {
+
+	'facebookAuth' : {
+		'clientID' 		: '563791980437215', // your App ID
+		'clientSecret' 	: 'b52f1ff9b93a229993d642376cda1f6d', // your App Secret
+		'callbackURL' 	: 'http://localhost:8080/auth/facebook/callback'
+	},
+
+	'twitterAuth' : {
+		'consumerKey' 		: 'your-consumer-key-here',
+		'consumerSecret' 	: 'your-client-secret-here',
+		'callbackURL' 		: 'http://localhost:8080/auth/twitter/callback'
+	},
+
+	'googleAuth' : {
+		'clientID' 		: 'your-secret-clientID-here',
+		'clientSecret' 	: 'your-client-secret-here',
+		'callbackURL' 	: 'http://localhost:8080/auth/google/callback'
+	}
+
+};
